af8fb74f7ddd7f6fa96fc16e2a8e03bbb7a1d6a4
agallenberger <35540612+agallenberger@users.noreply.github.com>
2018-03-10 17:07:56 -0800

    update neural net

--- neural_net.m
+++ neural_net.m
@@ -5,11 +5,13 @@ clear; clc; close all;
 load mnistdata;
 
 %% Initialize neural net parameters
+digit = 5;
 train = 1; %if 0, will use test digit instead
 layers = 1; %number of hidden layers
 
 %% Create neural network using neuron.m
-n = length(train0(1,:));
+input = double(logical(getMNISTdata(digit,train)));
+n = length(input(1,:));
 
 %Part 5 - initialize weight matrix. Each row sums to 1.
 W = rand(n,n);
@@ -17,11 +19,11 @@ for i = 1:n
     W(i,:) = W(i,:)./sum(W(i,:));
 end
 
-for k = 0:9
-    input = double(logical(getMNISTdata(k,train)));
-    OUT = zeros(n,1);
+%Train neural net on each digit
+OUT = zeros(n,1);
+for j = 1:layers
     for i = 1:n
         OUT(i) = neuron(input(1,:), W(i,:));
     end
-    fprintf('digit %1.0f -> average OUT = %1.7f\n', k, mean(OUT));
+    fprintf('digit %1.0f, pass #%1.0f -> average OUT = %1.7f\n', digit, j, mean(OUT));    
 end
